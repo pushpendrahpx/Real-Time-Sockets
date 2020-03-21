d0f52470c7d5aa2805f2105579efb2366b807cf5
Pushpendra Vishwakarma <48829314+Pushpendrahpx@users.noreply.github.com>
2020-03-21 15:42:14 +0530

    All Websockets Chat App And Real Time Google Search is Merged

--- index.js
+++ index.js
@@ -1,12 +1,100 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
+const express = require("express");
+
+const apps = express();
+
+let data =  require('./posts.json');
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://user:user@cluster0-m1e66.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
 
-app.listen(80);
+// admin.initializeApp(functions.config().firebase);
+
+// let db = admin.firestore();
+// let docRef = db.collection('users').doc('alovelace');
+
+// let setAda = docRef.set({
+//   first: 'Ada',
+//   last: 'Lovelace',
+//   born: 1815
+// });
+// let aTuringRef = db.collection('users').doc('aturing');
+
+// let setAlan = aTuringRef.set({
+//   'first': 'Alan',
+//   'middle': 'Mathison',
+//   'last': 'Turing',
+//   'born': 1912
+// });
+
+// db.collection('users').get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, '=>', doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
+
+
+const Post = mongoose.model('Post',
+{
+  "id": String,
+  "page_id": Number,
+  "name": String,
+  "message": String,
+  "description": String,
+  "caption": String,
+  "post_type": String,
+  "status_type": String,
+  "likes_count": Number,
+  "comments_count": Number,
+  "shares_count": Number,
+  "love_count": Number,
+  "wow_count": Number,
+  "haha_count": Number,
+  "sad_count": Number,
+  "thankful_count": Number,
+  "angry_count": Number,
+  "link":String,
+  "picture":String,
+  "posted_at": String
+});
+
+console.log("\n\n Going to Start PORT = 80 \n\n");
+bodyParser = require('body-parser');
+apps.use('/something',express.static('index.html'))
+// support parsing of application/json type post data
+apps.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+apps.use(bodyParser.urlencoded({ extended: true }));
+
+
+apps.listen(80);
+
+apps.get('/search/:term',(req,res)=>{
+  console.log("Got A Request")
+  console.log(req.params)
+  console.log(req.body)
+  let { term } = req.params;
+
+Post.find({ name:  {$regex: term} }).limit(10).exec( function (err, docs) { 
+  res.status(200).json(docs);
+  console.log("Throws")
+});
+
+
+
+
+})
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -20,10 +108,10 @@ function handler (req, res) {
     res.end(data);
   });
 }
-const Cat = mongoose.model('Cat', { name: String });
-Cat.find((e,ss)=>{
-    console.log(ss)
-})
+// const Cat = mongoose.model('Cat', { name: String });
+// Cat.find((e,ss)=>{
+//     console.log(ss)
+// })
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
@@ -44,7 +132,7 @@ io.on('connection', function (socket) {
     var m = e.getTime();
         Cat.find((err,chats)=>{
             console.log(chats)
-            socket.emit('ne',{chats:chats});
+            io.emit('ne',{chats:chats});
         });
 
         
